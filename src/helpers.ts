fee47c13a59842e194aace2ea7da2b8ac2ea7a0f
Daniel Andújar <daniandujar@gmail.com>
2023-01-17 11:12:37 +0100

    feat: send message to teams channel

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,25 @@
 import { Organization, Team } from '@kyso-io/kyso-model'
-import { AxiosResponse } from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 export const sendMessageToTeamsChannel = async (organization: Organization, team: Team, text: string): Promise<AxiosResponse<any>> => {
-    return null
+    let url: string = null
+    if (team?.teamsIncomingWebhookUrl) {
+        url = team.teamsIncomingWebhookUrl
+    } else if (organization?.options?.notifications?.teamsIncomingWebhookUrl) {
+        url = organization.options.notifications.teamsIncomingWebhookUrl
+    } else {
+        return
+    }
+    const axiosResponse: AxiosResponse<any> = await axios.post(
+        url,
+        {
+            text,
+        },
+        {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        },
+    )
+    return axiosResponse
 }
